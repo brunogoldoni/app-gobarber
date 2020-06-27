c7cad06ac74e16e1e3752e2afd94bef6a96557a9
Bruno <bgoldoni@valid.com.br>
2020-06-27 12:44:46 -0300

    components - input & button

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled(RectButton)`
+    align-items: center;
+    background: #ff9000;
+    border-radius: 10px;
+    height: 60px;
+    justify-content: center;
+    margin-top: 20px;
+    width: 100%;
+`;
+export const ButtonText = styled.Text`
+    color: #312e38;
+    font-family: 'RobotoSlab-Medium';
+    font-size: 18px;
+`;
